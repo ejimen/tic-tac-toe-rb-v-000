28e5d5d39164b3147c912a1a3fb31ecd2446d016
Elizabeth Jimenez <lizliz123@gmail.com>
2018-05-11 20:47:22 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -2,3 +2,12 @@
 def initialize(board = nil)
     @board = board || Array.new(9, " ")
   end
+
+  def display_board
+      puts " #{@board[0]} | #{@board[1]} | #{@board[2]} "
+      puts "-----------"
+      puts " #{@board[3]} | #{@board[4]} | #{@board[5]} "
+      puts "-----------"
+      puts " #{@board[6]} | #{@board[7]} | #{@board[8]} "
+    end
+
