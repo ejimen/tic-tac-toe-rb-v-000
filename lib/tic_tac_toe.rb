4101031cc10a3b4dfc74491c708099abb08fc108
Elizabeth Jimenez <lizliz123@gmail.com>
2018-05-11 20:48:22 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -10,3 +10,16 @@ def initialize(board = nil)
       puts "-----------"
       puts " #{@board[6]} | #{@board[7]} | #{@board[8]} "
     end
+
+    WIN_COMBINATIONS = [
+    [0, 1, 2], # Top row
+    [3, 4, 5], # Middle row
+    [6, 7, 8], # Bottom row
+    [0, 3, 6], # Left vertical
+    [1, 4, 7], # Middle vertical
+    [2, 5, 8], # Right vertical
+    [0, 4, 8], # Upper left diagonal
+    [2, 4, 6] # Upper right diagonal
+    ]
+
+  end
